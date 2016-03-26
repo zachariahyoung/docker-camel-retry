8c76a29d87a3b2a5f0a2a5b6b653d9cb74397d52
Zachariah Young <zpyoung@gmail.com>
2016-03-26 07:28:24 -0500

    Initial commit

---
+++ src/main/java/com/zandroid/dockercamelretry/DockerCamelRetryApplication.java
@@ -0,0 +1,12 @@
+package com.zandroid.dockercamelretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCamelRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCamelRetryApplication.class, args);
+	}
+}
